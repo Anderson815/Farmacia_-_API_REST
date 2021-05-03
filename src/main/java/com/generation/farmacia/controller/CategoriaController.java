7b688b76e229a403baabe9c996804100dab5dbeb
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-05-03 11:34:43 -0300

    feat: feito o método get findByIdCategoria

--- src/main/java/com/generation/farmacia/controller/CategoriaController.java
+++ src/main/java/com/generation/farmacia/controller/CategoriaController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +26,13 @@ public class CategoriaController {
 	public ResponseEntity<List<Categoria>> findAllCategoria(){
 		return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> findByIdCategoria(@PathVariable(value = "id") long id){
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	
 }
