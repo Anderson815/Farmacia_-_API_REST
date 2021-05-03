4242f60e775ea12810a52d569f2af19be5cd4464
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-05-03 11:22:49 -0300

    feat(CategoriaController): foi feito o método get findAllCategoria

---
+++ src/main/java/com/generation/farmacia/controller/CategoriaController.java
@@ -0,0 +1,28 @@
+package com.generation.farmacia.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.farmacia.model.Categoria;
+import com.generation.farmacia.repository.CategoriaRepository;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/categoria")
+public class CategoriaController {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> findAllCategoria(){
+		return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
+	}
+}
