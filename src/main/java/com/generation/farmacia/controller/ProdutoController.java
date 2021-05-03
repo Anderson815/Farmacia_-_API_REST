7b59077da88d5144714861d3d3e5aa69f54d534c
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-05-03 18:54:54 -0300

    feat: feito o método deleteProduto

--- src/main/java/com/generation/farmacia/controller/ProdutoController.java
+++ src/main/java/com/generation/farmacia/controller/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,5 +51,10 @@ public class ProdutoController {
 	@PutMapping
 	public ResponseEntity<Produto> putProduto(@RequestBody Produto produto){
 		return new ResponseEntity<>(repository.save(produto), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteProduto(@PathVariable(value = "id") long id) {
+		repository.deleteById(id);
 	}
  }
