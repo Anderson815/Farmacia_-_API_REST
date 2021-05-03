9b32fe00d1ef9d325677a1fe69f4b34c5381ac2d
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-05-03 10:52:26 -0300

    start: configuração de banco

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,49 @@
+package com.generation.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(max = 35)
+	private String nome;
+	
+	
+	private String descricao;
+
+	//Getters e Setters
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
