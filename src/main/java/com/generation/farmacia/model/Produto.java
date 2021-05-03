1718b76f7ebc767a34c0b5e9e68f467398bfd198
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-05-03 15:44:00 -0300

    feat: criado a entidade e o repositorio Produto

--- src/main/java/com/generation/farmacia/model/Produto.java
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -1,36 +1,41 @@
 package com.generation.farmacia.model;
 
-import java.util.List;
+import java.math.BigDecimal;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-public class Categoria {
+public class Produto {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
 	@NotNull
-	@Size(max = 35)
+	@Size(min = 3, max = 35)
 	private String nome;
 	
-	private String descricao;
-
-	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
-	@JsonIgnoreProperties("categoria")
-	private List<Produto> produtos;
+	@NotNull
+	private BigDecimal preco;
 	
+	@NotNull
+	private int quantidade;
+	
+	@NotNull
+	@ManyToOne
+	@JsonIgnoreProperties("produtos")
+	private Categoria categoria;
+
 	//Getters e Setters
+	
 	public long getId() {
 		return id;
 	}
@@ -47,12 +52,28 @@ public class Categoria {
 		this.nome = nome;
 	}
 
-	public String getDescricao() {
-		return descricao;
+	public BigDecimal getPreco() {
+		return preco;
 	}
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
 	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+//	public Categoria getCategoria() {
+//		return categoria;
+//	}
+//
+//	public void setCategoria(Categoria categoria) {
+//		this.categoria = categoria;
+//	}
 	
 }
